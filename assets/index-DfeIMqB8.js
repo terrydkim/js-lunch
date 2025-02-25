5ef32ac5dc1d2cbba372a4e63efc8171f65ad013
Terry <kdjtiger@gmail.com>
2025-02-25 23:51:54 +0900

    Updates

--- assets/index-DfeIMqB8.js
+++ assets/index-DfeIMqB8.js
@@ -42,7 +42,7 @@ const createHeader = (title = "점심 뭐 먹지") => {
   `
     <h1 class="gnb__title text-title">${title}</h1>
     <button type="button" class="gnb__button" aria-label="음식점 추가">
-      <img src="./public/assets/add-button.png" alt="음식점 추가">
+      <img src="/public/assets/add-button.png" alt="음식점 추가">
     </button>
   `;
   return header;
@@ -100,12 +100,12 @@ const restaurantData = [
 ];
 const createFragment = () => document.createDocumentFragment();
 const CATEGORY_IMG_SRC = {
-  "한식": `../assets/category-korean.png`,
-  "중식": `../assets/category-chinese.png`,
-  "일식": `../assets/category-japanese.png`,
-  "양식": `../assets/category-western.png`,
-  "아시안": `../assets/category-asian.png`,
-  "기타": `../assets/category-etc.png`
+  "한식": `/assets/category-korean.png`,
+  "중식": `/assets/category-chinese.png`,
+  "일식": `/assets/category-japanese.png`,
+  "양식": `/assets/category-western.png`,
+  "아시안": `/assets/category-asian.png`,
+  "기타": `/assets/category-etc.png`
 };
 const createRestaurantListItem = ({
   category,
